20cd9e65229a09a8521a6078188e1342b1d87612
Victor Nakoryakov <nail.xx@gmail.com>
2015-09-30 18:39:20 +0300

    add: modules minification

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    uglify: {
+      amperka: {
+        files: [{
+          expand: true,
+          cwd: 'modules/@amperka',
+          src: '*.js',
+          dest: 'modules/@amperka',
+          ext: '.min.js'
+        }]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default', ['uglify']);
+};
