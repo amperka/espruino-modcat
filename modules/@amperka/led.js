9501a4ea71dcab81cd308c8323f1f969cb03ccc1
Victor Nakoryakov <nail.xx@gmail.com>
2015-10-01 16:47:47 +0300

    add: led blink

--- modules/@amperka/led.js
+++ modules/@amperka/led.js
@@ -4,6 +4,10 @@ var Led = function(pin) {
   this._on = false;
   this._brightness = 1.0;
 
+  this._blinkTimeoutID = null;
+  this._blinkOnTime = 0;
+  this._blinkOffTime = 0;
+
   this._pin.mode('output');
 };
 
@@ -12,6 +16,7 @@ Led.prototype.toggle = function() {
     return this.toggle(!this._on);
   }
 
+  this._clearBlink();
   this._on = !!arguments[0];
   this._update();
 
@@ -28,6 +33,41 @@ Led.prototype.turnOff = function() {
 
 Led.prototype.isOn = function() {
   return this._on;
+};
+
+Led.prototype._clearBlink = function() {
+  if (this._blinkTimeoutID) {
+    clearTimeout(this._blinkTimeoutID);
+    this._blinkTimeoutID = null;
+  }
+};
+
+Led.prototype._blinkOn = function() {
+  this._on = true;
+  this._update();
+  this._blinkTimeoutID = setTimeout(
+    this._blinkOff.bind(this),
+    this._blinkOnTime * 1000);
+};
+
+Led.prototype._blinkOff = function() {
+  this._on = false;
+  this._update();
+
+  if (this._blinkOffTime) {
+    this._blinkTimeoutID = setTimeout(
+      this._blinkOn.bind(this),
+      this._blinkOffTime * 1000);
+  } else {
+    this._blinkTimeoutID = null;
+  }
+};
+
+Led.prototype.blink = function(onTime, offTime) {
+  this._clearBlink();
+  this._blinkOnTime = onTime;
+  this._blinkOffTime = offTime;
+  this._blinkOn();
 };
 
 Led.prototype.brightness = function(value) {
