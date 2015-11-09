609a30657ccac644262619b1910885dff1cdb5b5
Victor Nakoryakov <nail.xx@gmail.com>
2015-11-09 18:49:42 +0300

    add: click & hold events to button

--- modules/@amperka/button.js
+++ modules/@amperka/button.js
@@ -1,7 +1,10 @@
 
-var Button = function(pin) {
+var Button = function(pin, opts) {
   this._pin = pin;
   this._pin.mode('input');
+
+  this._holdTime = opts ? opts.holdTime : null;
+  this._holdTimeoutID = null;
 
   this._watch();
 };
@@ -11,17 +14,35 @@ Button.prototype.read = function() {
 };
 
 Button.prototype._watch = function() {
-  var onChange = function(e) {
-    this.emit(e.state ? 'release' : 'press');
-  };
-
-  setWatch(onChange.bind(this), this._pin, {
+  setWatch(this._onChange.bind(this), this._pin, {
     repeat: true,
     edge: 'both',
     debounce: 10
   });
 };
 
-exports.connect = function(pin) {
-  return new Button(pin);
+Button.prototype._onChange = function(e) {
+  var pressed = !e.state;
+  var self = this;
+
+  if (this._holdTime && pressed) {
+    // emit hold event after timeout specified in options
+    this._holdTimeoutID = setTimeout(function() {
+      self.emit('hold');
+      self._holdTimeoutID = null;
+    }, this._holdTime * 1000);
+  } else if (!pressed) {
+    // emit click only if hold was not already emitted
+    if (this._holdTimeoutID) {
+      clearTimeout(this._holdTimeoutID);
+      this._holdTimeoutID = null;
+      this.emit('click');
+    }
+  }
+
+  this.emit(pressed ? 'press' : 'release');
+}
+
+exports.connect = function(pin, opts) {
+  return new Button(pin, opts);
 };
