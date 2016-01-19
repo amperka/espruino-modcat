fad78e88508fb2740851037c178efbb8e8dee965
Roman Sorokin <roman@amperka.ru>
2016-01-19 12:05:57 +0300

    DigitalLineSensor - триггеры, поменяны местами черный и белый

--- modules/@amperka/digital-line-sensor.js
+++ modules/@amperka/digital-line-sensor.js
@@ -1,15 +1,18 @@
 var DigitalLineSensor = function(pin) {
   this._pin = pin;
   this._pin.mode('input');
-  setWatch(DigitalLineSensor.prototype._watch, this._pin, {repeat: true});
+  var self = this;
+  setWatch(function(e){
+      self.emit(e.state ? 'black' : 'white');
+  }, this._pin, {
+    repeat: true,
+    edge: 'both',
+    debounce: 10
+  });
 };
 
 DigitalLineSensor.prototype.read = function() {
   return this._pin.read() ? 'black' : 'white';
-};
-
-DigitalLineSensor.prototype._watch = function(e) {
-  this.emit(e.state ? 'black' : 'white');
 };
 
 exports.connect = function(pin) {
