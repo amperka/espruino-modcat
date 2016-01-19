1dc424d8ad6b5a3e70b0c48d733d7a93896a1027
Roman Sorokin <roman@amperka.ru>
2016-01-19 11:07:06 +0300

    DigitalLineSensor

---
+++ modules/@amperka/digitallinesensor.js
@@ -0,0 +1,17 @@
+var DigitalLineSensor = function(pin) {
+  this._pin = pin;
+  this._pin.mode('input');
+  setWatch(DigitalLineSensor.prototype._watch(), this._pin, {repeat: true});
+};
+
+DigitalLineSensor.prototype.read = function() {
+  return this._pin.read() ? 'black' : 'white';
+};
+
+DigitalLineSensor.prototype._watch = function(e) {
+  this.emit(e.state ? 'black' : 'white');
+};
+
+exports.connect = function(pin) {
+  return new DigitalLineSensor(pin);
+};
