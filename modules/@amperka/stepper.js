82ee890196257e8cee63150387bcac8326965176
Roman Sorokin <roman@amperka.ru>
2016-04-06 12:05:06 +0300

    JSDocs, errors
    Исправление ошибок из код-ревью, использование JSDocs

--- modules/@amperka/stepper.js
+++ modules/@amperka/stepper.js
@@ -1,54 +1,51 @@
-
-var Stepper = function(stepPin, directionPin, opts) {
+/**
+ * Конструктор объекта stepper
+ * @constructor
+ * @param {Pin} stepPin - объект пина, при подаче напряжения двигатель * сдвигается на один шаг.
+ * @param {Pin} directionPin - объект пина. 1 - движение вперед, 0 - движение назад
+ * @param {Pin} enablePin - объект пина, 1 - на двигатель подается напряжение
+ * @param {Object} opts - объект со свойствами pps (количество шагов в секунду) и holdPower (значение ШИМ для удержания)
+ */
+var Stepper = function(stepPin, directionPin, enablePin, opts) {
   this._stepPin = stepPin;
   this._directionPin = directionPin;
+  this._enablePin = enablePin;
+  opts = opts || {};
 
-  this._delay = 5;
-  this._holdPower = 0;
-  this._enablePin;
-
-  if (opts && opts.delay) {
-    this._delay = opts.delay;
-  }
-
-  if (opts && opts.enable) {
-    this._enablePin = opts.enable;
-  }
-
-  if (this._enablePin && opts.hold) {
-    this._holdPower = opts.hold;
-  }
+  this._pps = opts.pps || 20;
+  this._holdPower = opts.enablePin || 0;
 
   this._stepPin.mode('output');
   this._directionPin.mode('output');
+  this._enablePin.mode('output');
 
-  if (this._enablePin) {
-    this._enablePin.mode('output');
-    this.power();
-  }
+  this.power(this._holdPower);
 
   this._intervalId = null;
 };
 
-// Функция ограничивает подачу тока на двигатель
-// до holdPower процентов
-Stepper.prototype.power = function(holdPower) {
+/**
+ * Регулирует ШИМ подачи питания на двигатель
+ * @param {float} power - Скважность ШИМ от 0 до 1
+ */
+Stepper.prototype.power = function(power) {
   if (this._intervalId !== null) {
     clearInterval(this._intervalId);
     this._intervalId = null;
   }
 
-  if (holdPower === undefined) {
-    holdPower = this._holdPower;
+  if (!power) {
+    power = this._holdPower;
   }
 
-  if (this._enablePin) {
-    analogWrite(this._enablePin, holdPower);
-  }
+  analogWrite(this._enablePin, power);
 };
 
-// Функция осуществляет поворот вала на steps шагов,
-// через каждые delay микросекунд.
+/**
+ * Проворачивает вал на step шагов, после чего выполняет callback.
+ * @param {integer} steps - количество шагов. При отрицательном значении происходит движение назад
+ * @param {function} callback - функция, выполняемая после проворота вала
+ */
 Stepper.prototype.rotate = function(steps, callback) {
   if (this._intervalId !== null) {
     clearInterval(this._intervalId);
@@ -59,14 +56,13 @@ Stepper.prototype.rotate = function(steps, callback) {
     steps = 1;
   }
 
-  if (steps < 0 && this._directionPin) {
-    steps = -1 * steps;
+  if (steps < 0) {
     digitalWrite(this._directionPin, 1);
   } else if (this._directionPin) {
     digitalWrite(this._directionPin, 0);
   }
 
-  this.power(100);
+  this.power(1);
 
   var self = this;
   self._intervalId = setInterval(function(){
@@ -74,15 +70,21 @@ Stepper.prototype.rotate = function(steps, callback) {
       digitalPulse(self._stepPin, 1, 1);
       steps--;
     } else {
-      if (callback !== undefined) {
+      if (callback) {
         callback();
-      } else {
-        self.power();
       }
+      self.power();
     }
   }, this._delay);
 };
 
-exports.connect = function(stepPin, directionPin, opts) {
-  return new Stepper(stepPin, directionPin, opts);
+/**
+ * Экспорт функции создания объекта Stepper
+ * @param {Pin} stepPin - объект пина, при подаче напряжения двигатель * сдвигается на один шаг.
+ * @param {Pin} directionPin - объект пина. 1 - движение вперед, 0 - движение назад
+ * @param {Pin} enablePin - объект пина, 1 - на двигатель подается напряжение
+ * @param {Object} opts - объект со свойствами pps (количество шагов в секунду) и holdPower (значение ШИМ для удержания)
+ */
+exports.connect = function(stepPin, directionPin, enablePin, opts) {
+  return new Stepper(stepPin, directionPin, enablePin, opts);
 };
