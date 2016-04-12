3483772702149a201a3b194b0b0bfefa82fc5513
Roman Sorokin <r.sorokin@r1000.ru>
2016-04-12 11:15:51 +0300

    Freq

--- modules/@amperka/motor.js
+++ modules/@amperka/motor.js
@@ -3,15 +3,12 @@ var Motor = function(opts) {
   this._pwmPin.mode('output');
   this._phasePin = opts.phasePin;
   this._phasePin.mode('output');
-  if (opts.freq !== undefined) {
-    this._freq = opts.freq;
-    analogWrite(this._pwmPin, 0, {freq: this._freq});
-  }
+  this._freq = opts.freq || 100;
+  analogWrite(this._pwmPin, 0, {freq: this._freq});
 };
 
 Motor.prototype.write = function(u) {
   u < 0 ? this._phasePin.write(1) : this._phasePin.write(0);
-  var self = this;
   analogWrite(self._pwmPin, E.clip(Math.abs(u), 0, 1));
 };
 
