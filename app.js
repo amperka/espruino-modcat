c062583dc5677751c726318703beac87120d30a2
Victor Nakoryakov <nail.xx@gmail.com>
2016-05-25 13:20:19 +0300

    add: boards.json proxying with update from Amperka’s json

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 
+var fs = require('fs');
 var express = require('express');
+var requestJson = require('request-json');
+var objectAssign = require('object-assign');
+
 var app = express();
 
 app.use('/modules', express.static(__dirname + '/modules'));
 app.use('/binaries', express.static(__dirname + '/binaries'));
-app.use('/json', express.static(__dirname + '/json'));
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
+});
+
+app.get('/json/boards.json', function(req, res) {
+  var client = requestJson.createClient('http://espruino.com/');
+  client.get('json/boards.json', function(err, _, originalJson) {
+    if (err) {
+      originalJson = {};
+    }
+
+    fs.readFile(__dirname + '/json/boards.json', function(err, data) {
+      var localJson = JSON.parse(data);
+      var resultJson = objectAssign(originalJson, localJson);
+      res.send(resultJson);
+    });
+  });
+});
+
+app.get('/json/*', function(req, res){
+  res.sendFile(req.params[0], {root: __dirname + '/json'});
 });
 
 // The 404 Route
