ac2a6ef97e78f89af2f63ddfcf922b59efaa8786
Roman Sorokin <roman@amperka.ru>
2016-06-08 18:04:51 +0300

    Фичи преобразования величин

--- modules/imu10.js
+++ modules/imu10.js
@@ -2,84 +2,64 @@
  * Библиотека для работы с IMU10 модулем, реализующая ИНС
  */
 
+var IMU10 = function(opts) {
+  opts = opts || {};
+  this._i2c = opts.i2c || PrimaryI2C;                 // Шина I2C
+  this._baro = opts.baro || 0x5C;                     // Адрес бароматра
+  this._accl = opts.accl || 0x18;                     // Адрес акселерометра
+  this._gyro = opts.gyro || 0x68;                     // Адрес гироскопа
+  this._magn = opts.magn || 0x1C;                     // Адрес магнетометра
 
+  this._zeroPressure = undefined;                     // Давление на уровне земли
+  this._intId = undefined;                            // ID интервала
 
-var IMU10 = function(i2c) {
-  this._i2c = i2c || PrimaryI2C;
+  // Настроим I2C
   this._i2c.setup({sda: SDA, scl: SCL, bitrate: 400000});
-  this._baro = 0x5C;    // Адрес бароматра
-  this._accl = 0x18;    // Адрес акселерометра
-  this._gyro = 0x68;    // Адрес гироскопа
-  this._magn = 0x1C;    // Адрес магнетометра
-  this._baroAlt = 760;  // Текущее давление для высоты относительно инициализации
 };
 
-// Метод выводит устройства из состояния сна,
-// дожидается окончания проверки устройств,
-// запоминает текущее давление и вызывает callback по окончанию.
-IMU10.protottype.init = function(callback) {
-  this._writeI2C(this._baro, 0x20, 0xE0);   // Пробуждаем барометр и настраиваем частоту в 12.5Гц
+IMU10.prototype.setup = function() {
+  this._writeI2C(this._baro, 0x20, 0xE0);   // 0b11100000 - включаем барометр на частоте в 12.5Гц
 
-  this._writeI2C(this._accl, 0x20, 0x27);   // Пробуждаем акселерометр и настраиваем на частоту 100Гц
-  this._writeI2C(this._accl, 0x21, 0x10);   // Включаем высокочастотный фильтра с параметром 8
-  this._writeI2C(this._accl, 0x23, 0x11);   // Настраиваем чувствительность на 4G (не истребитель)
-
-
+  this._writeI2C(this._accl, 0x20, 0x37);   // 0b00110111 - включаем акселерометр на частоте 400Гц
+  this._writeI2C(this._accl, 0x21, 0x00);   // 0b00010001 - включаем ФВЧ с коэфициентом 16
+  this._writeI2C(this._accl, 0x23, 0x10);   // 0b00010000 - периодичное считывание, максимум 4G
 };
 
-IMU10.prototype.baro = function() {
-  var data = this.readI2C(0x28, 3);
-  var baro = data[1] << 8 | (data[2] << 16) | data[0];
-  if (baro >2147483647) {
-    baro -= 0xFFFFFFFF;
-  }
-  return baro;
-};
 
-IMU10.prototype.accel = function() {
-  var coef = 1/8192; // 4 / 32767;
-  var d = this._readI2C(this_accl, 0x28, 6);
-  // reconstruct 16 bit data
-  var res = {
-    x: d[0] | (d[1] << 8),
-    y: d[2] | (d[3] << 8),
-    z: d[4] | (d[5] << 8)
-  };
-
-  if (res.x >= 32767) {
-    res.x -=65536;
-  }
-  if (res.y >= 32767) {
-    res.y -=65536;
-  }
-  if (res.z >= 32767) {
-    res.z -=65536;
-  }
-
-  res = {
-    x: res.x * coef,
-    y: res.y * coef,
-    z: res.z * coef
-  };
-  return res;
-};
-
-// Метод читает из I2C приборчика с адресом addr
-// данные в размере count байт начиная с reg
+// Метод читает регистры, начиная с reg, приборчика addr, данные в количестве count байт
 IMU10.prototype._readI2C = function(addr, reg, count){
-  if (!count) {
-    count = 1;
-  }
+  count = count || 1;
   this._i2c.writeTo(addr, reg | 0x80);
   return this._i2c.readFrom(addr, count);
 };
 
-// Метода записывает в I2C приборчик с адресом addr
-// данные data, начиная с reg
+// Метода записывает в регистры, начиная с reg приборчика addr данные data
 IMU10.prototype._writeI2C = function(addr, reg, data) {
   this._i2c.writeTo(addr, [reg, data]);
 };
 
-exports.connect = function(i2c) {
-  return new IMU10(i2c);
+// Возвращает данные в гектоПаскалях
+IMU10.prototype.baro = function() {
+  var data = this._readI2C(this._baro, 0x28, 3);
+  var baro = new Uint32Array(data.buffer, 0, 1);
+  return baro[0] / 4096;
+};
+
+// Возвращает данные в коэфициенте G
+IMU10.prototype.accl = function() {
+  var coef = 4 / 32767;
+  var data = this._readI2C(this._accl, 0x28, 6);
+  var accl = new Int16Array(data.buffer, 0, 3);
+
+  var res = {
+    x: accl[0] * coef,
+    y: accl[1] * coef,
+    z: accl[2] * coef
+  };
+  return res;
+};
+
+
+exports.connect = function(opts) {
+  return new IMU10(opts);
 };
