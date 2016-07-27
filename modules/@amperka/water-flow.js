5aca269413b13d5c54cb79e2f95fcefb19180321
niggor <zotov.bmstu@gmail.com>
2016-07-27 12:26:28 +0300

    new commit

--- modules/@amperka/water-flow.js
+++ modules/@amperka/water-flow.js
@@ -15,10 +15,10 @@ var WaterFlow = function(pin, opts) {
 
   this._avg = opts.averageLength || 10;
   this._pulsesPerLitre = opts.pulsesPerLitre || 450;
-  this._minimumSpeed = opts.minimumSpeed || 1;
+  this._minimumSpeed = opts.minimumSpeed || 5;
 
   this._litresPerPulse = 1 / this._pulsesPerLitre;
-  this._speedNumerator = this._litresPerPulse * this._avg;
+  // this._speedNumerator = this._litresPerPulse * this._avg;
   this._updatePeriod = (60 * 1000) / (this._minimumSpeed * this._pulsesPerLitre);
 
   this._avgArray = new Array(this._avg); // [litres per second]
@@ -30,41 +30,28 @@ var WaterFlow = function(pin, opts) {
 };
 
 WaterFlow.prototype._watch = function() {
-  setWatch(this._onChange.bind(this), this._pin, {
+  var self = this;
+  setWatch(function(e) {
+    self._onChange(e);
+  }, this._pin, {
     repeat: true,
-    edge: 'rising',
-    debounce: 1
+    edge: 'rising'
   });
 };
 
-WaterFlow.prototype._average = function() {
-
-  this._avgArray[this._avgIterator] = getTime();
-
-  var last;
-  if (this._avgIterator === this._avg - 1) {
-    last = this._avgArray[0];
-  } else {
-    last = this._avgArray[this._avgIterator + 1];
-  }
-
-  var speed = this._speedNumerator / (this._avgArray[this._avgIterator] - last);
-
-  if (++this._avgIterator === this._avg) {
-    this._avgIterator = 0;
-  }
-
-  return speed;
-};
-
-WaterFlow.prototype._onChange = function() {
+WaterFlow.prototype._onChange = function(e) {
   this._pulses++;
   this._litres += this._litresPerPulse;
 
   if (this._pulseTimerID !== null) {
     clearTimeout(this._pulseTimerID);
     this._pulseTimerID = null;
-    this._speed = this._average();
+
+    this._avgArray[this._avgIterator++] = this._litresPerPulse / (e.time - e.lastTime);
+    // this._avgArray[this._avgIterator++] = getTime();
+    if (this._avgIterator === this._avg) {
+      this._avgIterator = 0;
+    }
   }
 
   var self = this;
@@ -87,20 +74,37 @@ WaterFlow.prototype.volume = function(units) {
 };
 
 WaterFlow.prototype.reset = function() {
-  var time = getTime();
+  // var time = getTime();
   for (var i = 0; i < this._avg; ++i) {
-    this._avgArray[i] = time;
+    this._avgArray[i] = 0;
+    // this._avgArray[i] = time;
   }
   this._litres = 0;
   this._pulses = 0;
 };
 
 WaterFlow.prototype.speed = function(units) {
+
+  var speed = 0;
+  for (var i = 0; i < this._avg; ++i) {
+    speed = this._avgArray[i];
+  }
+  speed /= this._avg;
+
+  // var last, curr;
+  // curr = this._avgArray[this._avgIterator];
+  // if (this._avgIterator === this._avg - 1) {
+  //   last = this._avgArray[0];
+  // } else {
+  //   last = this._avgArray[this._avgIterator + 1];
+  // }
+  // var speed = this._speedNumerator / (curr - last);
+
   switch (units) {
-    case 'l/min': return this._speed * 60;
-    case 'cm^3/min': return this._speed * 60 * 1000;
-    case 'm^3/min': return this._speed * 60 / 1000;
-    default: return this._speed * 60;
+    case 'l/min': return speed * 60;
+    case 'cm^3/min': return speed * 60 * 1000;
+    case 'm^3/min': return speed * 60 / 1000;
+    default: return speed * 60;
   }
 };
 
