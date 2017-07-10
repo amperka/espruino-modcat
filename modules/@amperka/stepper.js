c29cd92d612390c6b18dc7d8a86e18d78b7452ba
tolikivanov <photo.tolikivanov@gmail.com>
2017-07-10 14:59:10 +0300

    Fixed design and hold function

--- modules/@amperka/stepper.js
+++ modules/@amperka/stepper.js
@@ -25,19 +25,18 @@ var Stepper = function(pins, opts) {
  * @param {float} power - Скважность ШИМ от 0 до 1
  */
 Stepper.prototype.hold = function(power) {
+  
   if (this._intervalId !== null) {
     clearInterval(this._intervalId);
     this._intervalId = null;
   }
 
-  if (!power) {
+  if (typeof(power) === 'undefined') {
     power = this._holdPower;
   }
 
   analogWrite(this._pins.enable, power);
 };
-
-
 /**
  * Проворачивает вал на step шагов, после чего выполняет callback.
  * @param {number} steps - количество шагов. При отрицательном значении происходит движение назад
@@ -71,12 +70,14 @@ Stepper.prototype.rotate = function(steps, callback) {
   }, 1000 / this._pps);
 };
 
+/**
+ * Регулирует количество шагов в секунду
+ */
 Stepper.prototype.pps = function(pps) {
-  if (pps) {
-    this._pps = pps || 20;
-  }
+  if (pps === undefined) return this._pps;
+  this._pps = pps;
   return this;
-};
+}
 
 /**
  * Экспорт функции создания объекта Stepper
