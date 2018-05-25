190728ee304054f07056735f8d09ba236ac6d627
PSVM-J <39494832+PSVM-J@users.noreply.github.com>
2018-05-25 22:21:48 +0300

    fix tab

--- modules/@amperka/usb-keyboard.js
+++ modules/@amperka/usb-keyboard.js
@@ -7,36 +7,36 @@ E.setUSBHID({
     0x05, 0x01, // Usage Page (Generic Desktop),
     0x09, 0x06, // Usage (Keyboard),
     0xA1, 0x01, // Collection (Application),
-    0x75, 0x01, //   Report Size (1),
-    0x95, 0x08, //   Report Count (8),
-    0x05, 0x07, //   Usage Page (Key Codes),
-    0x19, 0xE0, //   Usage Minimum (224),
-    0x29, 0xE7, //   Usage Maximum (231),
-    0x15, 0x00, //   Logical Minimum (0),
-    0x25, 0x01, //   Logical Maximum (1),
-    0x81, 0x02, //   Input (Data, Variable, Absolute), ;Modifier byte
-    0x95, 0x01, //   Report Count (1),
-    0x75, 0x08, //   Report Size (8),
-    0x81, 0x03, //   Input (Constant),         ;Reserved byte
-    0x95, 0x05, //   Report Count (5),
-    0x75, 0x01, //   Report Size (1),
-    0x05, 0x08, //   Usage Page (LEDs),
-    0x19, 0x01, //   Usage Minimum (1),
-    0x29, 0x05, //   Usage Maximum (5),
-    0x91, 0x02, //   Output (Data, Variable, Absolute), ;LED report
-    0x95, 0x01, //   Report Count (1),
-    0x75, 0x03, //   Report Size (3),
-    0x91, 0x03, //   Output (Constant),         ;LED report padding
-    0x95, 0x06, //   Report Count (6),
-    0x75, 0x08, //   Report Size (8),
-    0x15, 0x00, //   Logical Minimum (0),
-    0x25, 0x68, //   Logical Maximum(104),
-    0x05, 0x07, //   Usage Page (Key Codes),
-    0x19, 0x00, //   Usage Minimum (0),
-    0x29, 0x68, //   Usage Maximum (104),
-    0x81, 0x00, //   Input (Data, Array),
+    0x75, 0x01, //  Report Size (1),
+    0x95, 0x08, //  Report Count (8),
+    0x05, 0x07, //  Usage Page (Key Codes),
+    0x19, 0xE0, //  Usage Minimum (224),
+    0x29, 0xE7, //  Usage Maximum (231),
+    0x15, 0x00, //  Logical Minimum (0),
+    0x25, 0x01, //  Logical Maximum (1),
+    0x81, 0x02, //  Input (Data, Variable, Absolute), ;Modifier byte
+    0x95, 0x01, //  Report Count (1),
+    0x75, 0x08, //  Report Size (8),
+    0x81, 0x03, //  Input (Constant),     ;Reserved byte
+    0x95, 0x05, //  Report Count (5),
+    0x75, 0x01, //  Report Size (1),
+    0x05, 0x08, //  Usage Page (LEDs),
+    0x19, 0x01, //  Usage Minimum (1),
+    0x29, 0x05, //  Usage Maximum (5),
+    0x91, 0x02, //  Output (Data, Variable, Absolute), ;LED report
+    0x95, 0x01, //  Report Count (1),
+    0x75, 0x03, //  Report Size (3),
+    0x91, 0x03, //  Output (Constant),     ;LED report padding
+    0x95, 0x06, //  Report Count (6),
+    0x75, 0x08, //  Report Size (8),
+    0x15, 0x00, //  Logical Minimum (0),
+    0x25, 0x68, //  Logical Maximum(104),
+    0x05, 0x07, //  Usage Page (Key Codes),
+    0x19, 0x00, //  Usage Minimum (0),
+    0x29, 0x68, //  Usage Maximum (104),
+    0x81, 0x00, //  Input (Data, Array),
     0xc0 // End Collection
-    ]
+  ]
 });
 
 // 1 = modifiers
@@ -107,7 +107,7 @@ var KEY = {
   "=": 46,
   "[": 47,
   "]": 48,
-  //"\\"    : 49, // minification problem here
+  //"\\"  : 49, // minification problem here
   BACKSLASH: 49,
   NUMBER: 50,
   ";": 51,
@@ -191,17 +191,17 @@ var tap = function(key, callback) {
   if (Array.isArray(key)) {
     for (var i = 0; i < key.length - 1; ++i) {
       modifiers |= key[i];
+    }
+    key = key[i];
   }
-  key = key[i];
-}
-if (typeof key === 'string') key = KEY[key];
-key = key || 0;
+  if (typeof key === 'string') key = KEY[key];
+  key = key || 0;
 
-E.sendUSBHID([modifiers, 0, key, 0, 0, 0, 0, 0]);
-setTimeout(function() {
+  E.sendUSBHID([modifiers, 0, key, 0, 0, 0, 0, 0]);
+  setTimeout(function() {
     E.sendUSBHID([0, 0, 0, 0, 0, 0, 0, 0]);
     if (callback) setTimeout(callback, 10);
-}, 10);
+  }, 10);
 }
 
 var type = function(txt, sendSpeed, callback) {
@@ -210,32 +210,32 @@ var type = function(txt, sendSpeed, callback) {
   var timeForWait;
   if (typeof(sendSpeed) === 'number' && sendSpeed < 50) {
     timeForWait = 1000 / sendSpeed;
-} else {
+  } else {
     timeForWait = 20;
-}
-var intr = setInterval(function() {
+  }
+  var intr = setInterval(function() {
     if (!txt.length) {
       clearInterval(intr);
       if (callback) callback();
-  } else {
+    } else {
       chr = txt[0];
       code = chr.charCodeAt(0);
       if (code >= 97 && code <= 122) {
         // lower case letter a-z
         tap(chr.toUpperCase());
-    } else if (code >= 65 && code <= 90) {
+      } else if (code >= 65 && code <= 90) {
         // upper case letter A-Z
         tap([MODIFY.SHIFT, chr]);
-    } else if (chr in KEY) {
+      } else if (chr in KEY) {
         tap(KEY[chr]);
-    } else if (chr in SHIFT_KEYS) {
+      } else if (chr in SHIFT_KEYS) {
         tap([MODIFY.SHIFT, SHIFT_KEYS[chr]]);
-    } else {
+      } else {
         tap('?');
+      }
+      txt = txt.substr(1);
     }
-    txt = txt.substr(1);
-}
-}, timeForWait);
+  }, timeForWait);
 }
 
 var pressedKeys = [0, 0, 0, 0, 0, 0, 0, 0];
@@ -244,36 +244,36 @@ var chooseKey = function(keyChanger, opts) {
   let chrCode;
   if (opts in MODIFY) {
     keyChanger([MODIFY[opts], 0]);
-} else {
+  } else {
     chrCode = opts.charCodeAt(0);
     if (chrCode >= 97 && chrCode <= 122) {
       // lower case letter a-z
       keyChanger(opts.toUpperCase());
-  } else if (chrCode >= 65 && chrCode <= 90) {
+    } else if (chrCode >= 65 && chrCode <= 90) {
       // upper case letter A-Z
       keyChanger(opts);
-  } else if (opts in KEY) {
+    } else if (opts in KEY) {
       keyChanger(KEY[opts]);
-  } else if (opts in SHIFT_KEYS) {
+    } else if (opts in SHIFT_KEYS) {
       keyChanger(SHIFT_KEYS[opts]);
-  } else {
+    } else {
       keyChanger('?');
+    }
   }
-}
 }
 
 var press = function(opts, callback) {
   chooseKey(changePressedKeys, opts);
   if (callback) {
     callback();
-}
+  }
 }
 
 var release = function(opts, callback) {
   chooseKey(changeReleasedKeys, opts);
   if (callback) {
     callback();
-}
+  }
 }
 
 var releaseAll = function() {
@@ -284,44 +284,44 @@ var releaseAll = function() {
 var changePressedKeys = function(opts) {
 
   var modifiers = pressedKeys[0];
-  var changeFlag = false; 
+  var changeFlag = false;
   if (Array.isArray(opts)) {
     // We've got an array of modifiers + key itself.
     // First combine modifiers
     for (var i = 0; i < opts.length - 1; ++i) {
       modifiers |= opts[i];
-  }
+    }
     // Then extract last element as a key
     opts = opts[i];
-}
-if (typeof opts === 'string') opts = KEY[opts];
+  }
+  if (typeof opts === 'string') opts = KEY[opts];
 
   // Protect from undefined
   opts = opts || 0;
 
-  for (var i = 2; i < 8; i++){
-    if(pressedKeys[i] === opts){
-      changeFlag = true; 
+  for (var i = 2; i < 8; i++) {
+    if (pressedKeys[i] === opts) {
+      changeFlag = true;
       break;
-  }
+    }
 
-}
-if (pressedKeys[0] !== modifiers || !changeFlag) {
+  }
+  if (pressedKeys[0] !== modifiers || !changeFlag) {
 
     if (pressedKeys[0] !== modifiers) {
       pressedKeys[0] = modifiers;
-  }
-  for (var i = 2; i < 8; i++) {
-    if (pressedKeys[i] === 0x00) {
+    }
+    for (var i = 2; i < 8; i++) {
+      if (pressedKeys[i] === 0x00) {
         pressedKeys[i] = opts;
         break;
+      }
     }
-}
-if (i === 8) {
-  return;
-}
-E.sendUSBHID(pressedKeys);
-}
+    if (i === 8) {
+      return;
+    }
+    E.sendUSBHID(pressedKeys);
+  }
 }
 var changeReleasedKeys = function(opts) {
   var changeFlag = false;
@@ -331,25 +331,25 @@ var changeReleasedKeys = function(opts) {
     // First combine modifiers
     for (var i = 0; i < opts.length - 1; ++i) {
       modifiers ^= opts[i];
-  }
+    }
     // Then extract last element as a key
     opts = opts[i];
-}
-if (typeof opts === 'string') opts = KEY[opts];
-opts = opts || 0;
-for (i = 2; i < 7; i++) {
+  }
+  if (typeof opts === 'string') opts = KEY[opts];
+  opts = opts || 0;
+  for (i = 2; i < 7; i++) {
     if (0 !== opts && pressedKeys[i] === opts) {
       pressedKeys[i] = 0x00;
       changeFlag = true;
+    }
   }
-}
-if (pressedKeys[0] !== modifiers) {
+  if (pressedKeys[0] !== modifiers) {
     pressedKeys[0] = modifiers;
     changeFlag = true;
-}
-if (changeFlag) {
+  }
+  if (changeFlag) {
     E.sendUSBHID(pressedKeys);
-}
+  }
 }
 exports.KEY = KEY;
 exports.MODIFY = MODIFY;
