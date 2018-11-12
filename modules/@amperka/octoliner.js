c24475653cff0041c493c619b069b4482b05e0bb
PSVM-J <alex@amperka.ru>
2018-11-12 14:21:17 +0300

    fix BinaryLine and other

--- modules/@amperka/octoliner.js
+++ modules/@amperka/octoliner.js
@@ -1,12 +1,20 @@
 var Octoliner = function(opts) {
   opts = opts || {};
+  if (opts.i2c === undefined) {
+    I2C1.setup({
+      sda: SDA,
+      scl: SCL,
+      bitrate: 100000
+    });
+    opts.i2c = I2C1;
+  }
   this.expander = require("@amperka/gpio-expander").connect(opts);
   this.expander.pwmFreq(60000);
   this._sensePin = 0;
   this._ledBrightnessPin = 9;
   this._sensorPinMap = [4, 5, 6, 8, 7, 3, 2, 1];
   this._lineState = [];
-  this.setSensitivity(0.8);
+  this.setSensitivity(0.91);
   this.setBrightness(1);
 };
 
@@ -23,7 +31,7 @@ Octoliner.prototype.analogRead = function(sensor) {
   return this.expander.analogRead(this._sensorPinMap[sensor]);
 };
 
-Octoliner.prototype.digitalRead = function(sensor){
+Octoliner.prototype.digitalRead = function(sensor) {
   sensor &= 0x07;
   return this.expander.digitalRead(this._sensorPinMap[sensor]);
 };
@@ -32,7 +40,7 @@ Octoliner.prototype.changeAddr = function(nAddr) {
   this.expander.changeAddr(nAddr);
 };
 
-Octoliner.prototype.saveAddr = function(){
+Octoliner.prototype.saveAddr = function() {
   this.expander.saveAddr();
 };
 
@@ -47,34 +55,34 @@ Octoliner.prototype.getBinaryLine = function(treshold) {
   return result;
 };
 
-Octoliner.prototype.mapLine = function(binaryLine) {
+Octoliner.prototype.mapLine = function(Line) {
   var sum = 0;
   var avg = 0;
   var weight = [4, 3, 2, 1, -1, -2, -3, -4];
-  if (Array.isArray(binaryLine)) {
+  if (Array.isArray(Line)) {
     for (var i = 0; i < 8; i++) {
-      if (binaryLine[i]) {
-        sum += binaryLine[i];
-        avg += binaryLine[i] * weight[i];
-      }
+      sum += Line[i];
+      avg += Line[i] * weight[i];
     }
     if (sum != 0) {
       return avg / sum / 4.0;
     }
-      return 0;
+    return 0;
   } else {
     this._lineState = [];
     for (var i = 0; i < 8; i++) {
       var mask = 1 << i;
-      if (binaryLine & mask) {
+      if (Line & mask) {
         this._lineState[i] = 1;
+      } else {
+        this._lineState[i] = 0;
       }
     }
     return this.mapLine(this._lineState);
   }
 };
 
-Octoliner.prototype.reset = function(){
+Octoliner.prototype.reset = function() {
   this.expander.reset();
 };
 
