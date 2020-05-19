ea16227f52a0a3b1391c4e97cbb3bd01210b94b6
Alexander Belov <ipritoflex@yandex.ru>
2020-05-19 15:39:41 +0300

    Trailing space
    Co-authored-by: Sergey Korolev <korolev.srg@gmail.com>

--- modules/@amperka/server.js
+++ modules/@amperka/server.js
@@ -40,7 +40,6 @@ Server.prototype._event = function(eventName, req, res) {
     res.writeHead(404, { 'Content-Type': 'text/html' });
     res.write('<h1>404 - Not found</h1>');
   }
-  
   res.end();
 };
 
